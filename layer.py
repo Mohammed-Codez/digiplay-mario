9f15d415754528cd709b021e02fee411a8c5fbd9
Mohammed-Codez <amakactive@gmail.com>
2026-04-27 18:19:12 +0400

    created activator.py, as well as modifying the layer to support customizable activator functions. if you check the stashes, you may notice that the nn.py files differs from the current file. but it's becuse numpy was demising me. also icloud hates me.

--- layer.py
+++ layer.py
@@ -1,40 +1,73 @@
 import numpy as np
 from node import Node
+from activator import *
+from typing import Callable
 
 
 class Layer:
-    def __init__(self, weights: list[list[float]], biases: list[float]) -> None:
-        self.weights: np.ndarray = np.array(weights)
-        self.biases: np.ndarray = np.array(biases)
+    def __init__(
+            self,
+            input_count: int,
+            node_count: int,
+            weights: list[list[float]] | None = None,
+            biases: list[float] | None = None,
+            activator: Callable[[float], float] = silu
+        ) -> None:
+        self.input_count: int = input_count
+        self.node_count: int = node_count
+
+        self.weights: np.ndarray = (
+            np.array(weights)
+            if weights is not None else
+            np.zeros((self.node_count, self.input_count))
+        )
+
+        self.biases: np.ndarray = (
+            np.array(biases)
+            if biases is not None
+            else np.zeros(self.node_count)
+        )
+
+        self.activator = activator
 
     def __str__(self) -> str:
-        return f"w: {self.weights}, b: {self.biases}"
+        return f"""
+i: {self.input_count},
+n: {self.node_count},
+w:
+{self.weights},
+b:
+{self.biases},
+a: {self.activator.__name__}
+"""
 
     def __repr__(self) -> str:
         return f"w: {self.weights}, b: {self.biases}"
+    
+    def generate_random_weights(self, min=-1, max=1) -> None:
+        self.weights = np.random.uniform(min, max, (self.node_count, self.input_count))
+
+    def generate_random_biases(self, min=-1, max=1) -> None:
+        self.biases = np.random.uniform(min, max, self.node_count)
+
+    def generate_zeroed_weights(self) -> None:
+        self.weights = np.zeros((self.node_count, self.input_count))
+
+    def generate_zeroed_biases(self) -> None:
+        self.biases = np.zeros(self.node_count)
 
     def calc(self, inputs: list[float]):
-        result = []
-        print(inputs)
-        for i in range(len(self.weights)):
-            node = Node(self.weights[i], self.biases[i])
-            print(node)
-            output = node.calc(inputs)
-            print(output)
-            result.append(output)
-
-        return result
+        return [
+            self.activator(np.dot(self.weights[i], inputs) + self.biases[i])
+            for i in range(self.node_count)
+        ]
 
 
-layer = Layer(
-    [
-        [0.1, 0.2],  # first node
-        [0.4, 0.3],  # second node
-    ],
-    [
-        0.8,  # first node
-        0.5,  # second node
-    ],
-)
-print(layer)
-print(layer.calc([0.7, 0.6]))
+if __name__ == "__main__":
+    layer = Layer(2, 3)
+    print(f'Layer Data: \n{layer}')
+    layer.generate_random_weights()
+    print(f'Layer Data after generating random weights: \n{layer}')
+    layer.generate_random_biases()
+    print(f'Layer Data after generating random biases: \n{layer}')
+    print(f'Layer Output: \n{layer.calc([1.0, 0.5])}')
---
+++ nn.py
@@ -0,0 +1,85 @@
+import numpy as np
+from random import uniform, randint
+from layer import Layer
+import pprint as pp
+from activator import *
+from typing import Callable
+
+
+class NeuralNet:
+    def __init__(
+            self,
+            input_count: int,
+            layer_sizes: list[int],
+            weights: list[list[list[float]]] | None = None,
+            biases: list[list[float]] | None = None
+        ) -> None:
+        self.input_count: int = input_count
+        self.layer_sizes: np.ndarray = np.array(layer_sizes)
+
+        '''
+        [
+           -- represent weights per node -->
+        | [[0.0 0.0]  |
+        |  [0.0 0.0]] V represent nodes per layer
+        | 
+        | [[0.0 0.0]
+        |  [0.0 0.0]]
+        V
+        represent layers per neural net
+        ]
+        '''
+
+        self.layers = [Layer(
+            input_count if i == 0 else layer_sizes[i - 1],
+            layer_sizes[i],
+            weights[i] if weights is not None else None, 
+            biases[i] if biases is not None else None
+        ) for i in range(len(layer_sizes))]
+
+    def __str__(self) -> str:
+        return f"""
+i: {self.input_count},
+ls: {self.layer_sizes},
+l:
+{pp.pformat(self.layers, depth=4)}
+"""
+    
+    def __repr__(self) -> str:
+        return f"""
+i: {self.input_count},
+ls: {self.layer_sizes},
+l:
+{pp.pformat(self.layers, depth=4)}
+"""
+    
+    def geneate_random_weights(self, min=-1, max=1) -> None:
+        for layer in self.layers:
+            layer.generate_random_weights(min, max)
+    
+    def generate_random_biases(self, min=-1, max=1) -> None:
+        for layer in self.layers:
+            layer.generate_random_biases(min, max)
+
+    def generate_zeroed_weights(self) -> None:
+        for layer in self.layers:
+            layer.generate_zeroed_weights()
+
+    def generate_zeroed_biases(self) -> None:
+        for layer in self.layers:
+            layer.generate_zeroed_biases()
+
+    def calc(self, inputs: list[float]) -> list[float]:
+        for layer in self.layers:
+            inputs = layer.calc(inputs)
+        return inputs
+
+if __name__ == "__main__":
+    nn = NeuralNet(4, [3, 2])
+    print('NN Data: \n', nn)
+    nn.geneate_random_weights()
+    print('NN Data after generating random weights: \n', nn)
+    nn.generate_random_biases()
+    print('NN Data after generating random biases: \n', nn)
+    nn.layers[-1].activator = heaviside
+    print('NN Output: \n', nn.calc([1.0, 0.5, -1.5, 2.0]))
